6579eb295991037eb1f2785abf4cf87648a3f4fc
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-01-09 19:25:39 +0530

    Add the frame

--- src/pdfMaker.java
+++ src/pdfMaker.java
@@ -1,4 +1,26 @@
-
+import javax.swing.*;
 public class pdfMaker {
-
+	//Declarations
+	private JFrame frame;
+	
+	pdfMaker(){
+		setUpGUI();
+	}
+	
+	private void setUpGUI() {
+		//Initializations
+		frame = new JFrame("pdfMaker");
+		
+		//Setting up
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setSize(425, 425);
+		frame.setLocationRelativeTo(null);
+		
+	}
+	
+	public static void main (String args[]) {
+		new pdfMaker();
+	}
 }
